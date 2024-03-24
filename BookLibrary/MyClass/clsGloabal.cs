c0fdbd814ddc78ff6929f55ba95a2570fa77e885
Mujtaba Ibrahim <127867695+mujtaba98iq@users.noreply.github.com>
2024-03-25 02:34:55 +0300

    Add files via upload

---
+++ BookLibrary/MyClass/clsGloabal.cs
@@ -0,0 +1,27 @@
+﻿using BookLibrary.Forms.Books;
+using BookLibrary.Forms.Borrowing;
+using BookLibrary_BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.MyClass
+{
+    public class clsGloabal
+    {
+       static public clsUser CurrentUser;
+        public static string Mode = "LightMode";
+        public static frmBookLibrary BookForm;
+        public static frmManageReturns ReturnsForm;
+
+        public static int DefaultBorrowDays = 3;
+        public static int DefaultFinePerDay = 5;
+
+        public static Color BaseColor = Color.White;
+        public static Color SecondColor = Color.LightGray;
+
+    }
+}
